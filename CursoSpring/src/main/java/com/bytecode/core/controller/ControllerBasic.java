f13c543890e8a604c1dc9cc0571eb08654cb1516
EVERTECINC\ET60243 <eduardo.vicente@evertecinc.com>
2023-01-13 21:34:43 -0500

    Video 9 - 13/01/2023 21:34

--- CursoSpring/src/main/java/com/bytecode/core/controller/ControllerBasic.java
+++ CursoSpring/src/main/java/com/bytecode/core/controller/ControllerBasic.java
@@ -1,16 +1,33 @@
 package com.bytecode.core.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bytecode.core.model.Post;
+
 @Controller
-@RequestMapping("/hola")
+@RequestMapping("/home")
 public class ControllerBasic {
 	
-	@GetMapping({"/saludar","/holamundo"})
-	public String saludar() {
+	@GetMapping({"/post","/"})
+	public String saludar(Model model) {
+		model.addAttribute("posts", this.getPosts());
 		return "index";
 	}
 	
+	public List<Post> getPosts(){
+		ArrayList<Post> post = new ArrayList<>();
+		post.add(new Post(1, "Desarrollo web es un término que define la creación de sitios web para Internet o una intranet.","http://localhost/img/mario.jpg", new Date(),"Desarrollo Web"));
+		post.add(new Post(2, "Desarrollo web es un término que define la creación de sitios web para Internet o una intranet.","http://localhost/img/mario.jpg", new Date(),"Desarrollo Web Front End"));
+		post.add(new Post(3, "Desarrollo web es un término que define la creación de sitios web para Internet o una intranet.","http://localhost/img/mario.jpg", new Date(),"Desarrollo Web Back End"));
+		post.add(new Post(4, "Desarrollo web es un término que define la creación de sitios web para Internet o una intranet.","http://localhost/img/mario.jpg", new Date(),"Desarrollo Web UX/UI"));
+		
+		return post;
+	}
 }
